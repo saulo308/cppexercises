612a0106cf025999038e358a454ac2b88afcea71
Saulo Soares <saulosoares150498@gmail.com>
2020-03-30 18:10:29 -0300

    LinkedList

---
+++ LinkedList.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        Node* next;
+        int data;
+    Node(){
+        next = nullptr;
+    }
+    Node(int newData){
+        data = newData;
+        next = nullptr;
+    }
+    Node(int newData,Node* nextRef){
+        data = newData;
+        next = nextRef;
+    }
+};
+
+void PrintList(Node *list){
+    while(list != nullptr){
+        cout << list->data << endl;
+        list = list->next;
+    }
+}
+
+/* Node* Push(Node* curHead,int data){
+    return new Node(data,curHead);
+} */
+
+void Push(Node** curHead,int data){
+    Node* newNode = new Node(data);
+    newNode->next = *curHead;
+    (*curHead) = newNode;
+}
+
+void Append(Node* node,int data){
+    Node* newNode = new Node(data);
+    while(node->next != nullptr){
+        node = node->next;
+    }
+    node->next = newNode;
+}
+
+void InsertAfter(Node* prevNode,int data){
+    if(prevNode == nullptr) return;
+
+    Node* newNode = new Node(data);
+    newNode->next = prevNode->next;
+    prevNode->next = newNode;
+}
+
+int main(){
+    Node* head = new Node();
+    head->data = 2;
+
+    Node* second = new Node();
+    second->data = 3;
+
+    head->next = second;
+
+   PrintList(head);
+   Push(&head,4);
+   cout << endl;
+   PrintList(head);
+   cout<<endl;
+   Append(head,10);
+   PrintList(head);
+   cout <<endl;
+   InsertAfter(second,5);
+   PrintList(head);
+
+
+
+    return 0;
+}
